868395bd2da53ecc83e47af050b8c9d41849c131
frankydanielsinaga <frankydanielsinaga@gmail.com>
2019-12-03 12:20:18 +0700

    https://github.com/frankydanielsinaga/reactzalora.git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './style/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component{
+  render(){
+    return(
+      <div className="wraaper">
+        <div className="header">
+          <div className="navbar">
+            <div className="logo">
+              <img className="imgfluid" src={require('./logo/zalora-logo.png')} />
+            </div>
+            <div className="menu">
+             <ul>
+                <li><a>wanita</a></li>
+                <li><a>pria</a></li>
+                <li><a>anak-anak</a></li>
+             </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    )
+  }
+} 
 export default App;
